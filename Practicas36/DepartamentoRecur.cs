beff9bc92b59743121920b30101c1705bcb19503
practicas36 <practicas36@DIEMPI36>
2022-03-28 16:52:43 +0200

    Agregar archivos de proyecto.

---
+++ Practicas36/DepartamentoRecur.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Practicas36
+{
+    public partial class DepartamentoRecur : DbContext
+    {
+        public DepartamentoRecur()
+        {
+        }
+
+        public DepartamentoRecur(DbContextOptions<DepartamentoRecur> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Departments { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost;Database=AdventureWorks2019;User Id=SA;Password=#Practicas36");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.ToTable("Department", "HumanResources");
+
+                entity.HasComment("Lookup table containing the departments within the Adventure Works Cycles company.");
+
+                entity.HasIndex(e => e.Name, "AK_Department_Name")
+                    .IsUnique();
+
+                entity.Property(e => e.DepartmentId)
+                    .HasColumnName("DepartmentID")
+                    .HasComment("Primary key for Department records.");
+
+                entity.Property(e => e.GroupName)
+                    .HasMaxLength(50)
+                    .HasComment("Name of the group to which the department belongs.");
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())")
+                    .HasComment("Date and time the record was last updated.");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .HasComment("Name of the department.");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Practicas36/Program.cs
@@ -0,0 +1,10 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tabla RecursosHumanos.Departamento");
+Console.WriteLine("----------------------------------");
+Console.WriteLine("CREAR");
+Console.WriteLine("LEER");
+Console.WriteLine("MODIFICAR");
+Console.WriteLine("BORRAR");
+Console.WriteLine("SALIR");
+Console.WriteLine("----------------------------------");
